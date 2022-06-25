36f3adf180d0cb6edc539bfc96486a301c3ae0a0
noceive <nocivescience@gmail.com>
2022-06-24 23:14:32 -0400

    mi primer commit

---
+++ hangman.js
@@ -0,0 +1,20 @@
+const wordsList = [
+    'javascript',
+    'html',
+    'css',
+    'bootstrap',
+    'jquery',
+    'react',
+    'node',
+    'express',
+    'mongodb',
+    'mysql',
+    'sql',
+    'ajax',
+];
+const targetsEl=document.getElementsByClassName('targets');
+const wrongEl=[];
+const correctEl=[];
+function displayWord(word) {
+    
+}
